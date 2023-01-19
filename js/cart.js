17aceb1b47bbd5aa77dc2327384b69ecb9cf4e64
Shifat507 <ferdoushasanshifat@gmail.com>
2023-01-19 18:34:30 +0600

    added js to cart btn

---
+++ js/cart.js
@@ -0,0 +1,65 @@
+const itemSelect1 = document.getElementById('btn-1');
+itemSelect1.addEventListener('click',function(){
+    const selecteditems = document.getElementById('name-1');
+    const selecteditemsPrice = document.getElementById('p-1');
+    const show = selecteditems.innerText;
+    const showPrice = selecteditemsPrice.innerText
+    const itemselection = document.getElementById('item-1');
+    const priceSelection = document.getElementById('item-price-1');
+    itemselection.innerText = show;
+    priceSelection.innerText =showPrice;
+    document.getElementById('btn-1').disabled = true;
+})
+
+const itemSelect2 = document.getElementById('btn-2');
+itemSelect2.addEventListener('click',function(){
+    const selecteditems = document.getElementById('name-2');
+    const selecteditemsPrice = document.getElementById('p-2');
+    const show = selecteditems.innerText;
+    const showPrice = selecteditemsPrice.innerText
+    const itemselection = document.getElementById('item-2');
+    const priceSelection = document.getElementById('item-price-2');
+    itemselection.innerText = show;
+    priceSelection.innerText =showPrice;
+    document.getElementById('btn-2').disabled = true;
+})
+
+const itemSelect3 = document.getElementById('btn-3');
+itemSelect3.addEventListener('click',function(){
+    const selecteditems = document.getElementById('name-3');
+    const selecteditemsPrice = document.getElementById('p-3');
+    const show = selecteditems.innerText;
+    const showPrice = selecteditemsPrice.innerText
+    const itemselection = document.getElementById('item-3');
+    const priceSelection = document.getElementById('item-price-3');
+    itemselection.innerText = show;
+    priceSelection.innerText =showPrice;
+    document.getElementById('btn-3').disabled = true;
+})
+
+const itemSelect4 = document.getElementById('btn-4');
+itemSelect4.addEventListener('click',function(){
+    const selecteditems = document.getElementById('name-4');
+    const selecteditemsPrice = document.getElementById('p-4');
+    const show = selecteditems.innerText;
+    const showPrice = selecteditemsPrice.innerText
+    const itemselection = document.getElementById('item-4');
+    const priceSelection = document.getElementById('item-price-4');
+    itemselection.innerText = show;
+    priceSelection.innerText =showPrice;
+    document.getElementById('btn-4').disabled = true;
+})
+
+const itemSelect5 = document.getElementById('btn-5');
+itemSelect5.addEventListener('click',function(){
+    const selecteditems = document.getElementById('name-5');
+    const selecteditemsPrice = document.getElementById('p-5');
+    const show = selecteditems.innerText;
+    const showPrice = selecteditemsPrice.innerText
+    const itemselection = document.getElementById('item-5');
+    const priceSelection = document.getElementById('item-price-5');
+    itemselection.innerText = show;
+    priceSelection.innerText =showPrice;
+    document.getElementById('btn-5').disabled = true;
+})
+
